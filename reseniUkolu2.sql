cb3b1461022e52458884f3d5293cec4ef4d2cf16
Lucka256 <44521678+Lucka256@users.noreply.github.com>
2018-10-30 20:33:00 +0100

    Řešení úkolu č.2

--- reseniUkolu2.sql
+++ reseniUkolu2.sql
@@ -1,1 +1,34 @@
+--1.Vyber 10 programatoru, kteri jsou ve spolecnosti nejkratsi dobu. 
+select FIRST_NAME, LAST_NAME, JOB_TITLE, HIRE_DATE 
+from  
+( 
+    select FIRST_NAME, LAST_NAME, JOB_ID, HIRE_DATE 
+    from hr.employees 
+    where JOB_ID = 'IT_PROG' 
+    order by HIRE_DATE asc 
+) hire 
+left join hr.jobs on hire.JOB_ID = hr.jobs.JOB_ID
+where rownum <= 10
 
+--2.Vyber 10 Sales Manageru, kteri jsou ve spolecnosti nejdelsi dobu
+select FIRST_NAME, LAST_NAME, JOB_TITLE, HIRE_DATE 
+from  
+( 
+    select FIRST_NAME, LAST_NAME, JOB_ID, HIRE_DATE 
+    from hr.employees 
+    where JOB_ID = 'SA_MAN' 
+    order by HIRE_DATE desc 
+) hire 
+left join hr.jobs on hire.JOB_ID = hr.jobs.JOB_ID
+where rownum <= 10
+
+--3.Kolik firma mesicne vyplaci zamestnancum na vyplatach rozdeleno podle pozic.
+select JOB_TITLE, SUM_SALARY 
+from 
+( 
+    select JOB_ID, SUM(SALARY) as SUM_SALARY 
+    from hr.employees 
+   group by JOB_ID 
+   ) hire 
+left join hr.jobs on hire.JOB_ID = hr.jobs.JOB_ID 
+order by SUM_SALARY asc
